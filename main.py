c74b10493cd1de661bfa25d647894ee60e766821
thelazerpotaos <81989499+VahinNaik@users.noreply.github.com>
2021-10-18 23:37:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+a = input("what did you get?: ")
+b = input("out of?: ")
+percentage = (float(a)/float(b))*100
+if percentage<=49:
+  print("you failed!! :( (F)")
+elif percentage<=59:
+  print("get a toutor!!(D)") 
+elif percentage<=69:
+  print("study harder!!(C)")
+elif percentage<=79:
+  print("Not good enough!!(B)")
+elif percentage<=99:
+  print("Good enough(A)")
+elif percentage<=100:
+  print("Get a life NERD!!(A+)")
+else:
+  print("WTF how!!(A++)")
+print(str(percentage )+ "%")
